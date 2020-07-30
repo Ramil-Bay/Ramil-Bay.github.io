28fe50f505dc0376b89e21771392d94fee895fff
Ramil Baybulatov <ramilbajbulatov1488@gmail.com>
2020-07-30 16:16:57 +0300

    20th commit

--- site4/script.js
+++
@@ -1,5 +0,0 @@
-var read = document.querySelector('.read-more');
-var hidden = document.querySelector('.brands-hidden');
-read.addEventListener('click', function(){
-	hidden.classList.remove('brands-hidden')
-});
